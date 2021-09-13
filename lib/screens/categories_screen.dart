9af0c6dce5461667cb868b6fba5c8161e69dbf79
Walid Wazini <wazini25@gmail.com>
2021-09-13 15:59:47 +0800

    new main screen

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -9,16 +9,21 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate:SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200, childAspectRatio: 3 / 2,
-        mainAxisSpacing: 20, crossAxisSpacing: 20,
-      ) ,
+    return Scaffold(
+      appBar: AppBar(title: const Text('Meals'),),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+        ),
 
-      // Map dummy data(DUMMY_CATEGORIES) into the widget(Category Item)
-      children: DUMMY_CATEGORIES.map((catData){
-        return CategoryItem(catData.modelTitle, catData.modelColor);
-      }).toList(),
+        // Map dummy data(DUMMY_CATEGORIES) into the widget(Category Item)
+        children: DUMMY_CATEGORIES.map((catData) {
+          return CategoryItem(catData.modelTitle, catData.modelColor);
+        }).toList(),
+      ),
     );
   }
 }
