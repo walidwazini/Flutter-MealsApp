668f6fcff0234470a77e0fcdc2e4c437e4ae1e08
Walid Wazini <wazini25@gmail.com>
2021-09-21 13:09:20 +0800

    Connexting meals for categories

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/dummy_data.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
   //const CategoryMealsScreen({Key? key}) : super(key: key);
@@ -14,15 +16,18 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(konteks).settings.arguments as Map<String,dynamic>;
     final categoryTitle = routesArgs['title'];
     final categoryId = routesArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal){
+      return meal.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
         appBar: AppBar(
           title: Text(categoryTitle),
         ),
         body: Center(
-          // child: ListView.builder(itemBuilder: (ctx, index) {
-          //
-          // } ,itemCount:  ,),
+          child: ListView.builder(itemBuilder: (ctx, index) {
+            return Text(categoryMeals[index].title);
+          } ,itemCount: categoryMeals.length ,),
         ));
   }
 }
