30c8963acc05cc639359914b1d106af02bdd2904
Walid Wazini <wazini25@gmail.com>
2021-09-22 11:37:29 +0800

    finalized meal card widget

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -16,6 +16,39 @@ class MealItem extends StatelessWidget {
       @required this.affordability,
       @required this.complexity});
 
+  // Getter
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+        break;
+      case Complexity.Challenging:
+        return 'Challenging';
+        break;
+      case Complexity.Hard:
+        return 'Hard';
+        break;
+      default:
+        return 'Unknown';
+    }
+  }
+  // Getter
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+        break;
+      case Affordability.Pricey:
+        return 'Pricey';
+        break;
+      case Affordability.Luxurious:
+        return 'Luxurious';
+        break;
+      default:
+        return 'Unknown';
+    }
+  }
+
   void selectMeal() {}
 
   @override
@@ -44,12 +77,45 @@ class MealItem extends StatelessWidget {
                     fit: BoxFit.cover,
                   ),
                 ),
-                Text(
-                  title,
-                  style: TextStyle(fontSize: 26, color: Colors.white),
-                  softWrap: true, overflow: TextOverflow.fade,
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                    child: Text(
+                      title,
+                      style: TextStyle(fontSize: 26, color: Colors.white),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
                 )
               ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(children: [
+                    Icon(Icons.schedule),
+                    SizedBox(width: 6,),
+                    Text('$duration min')
+                    ],),
+                  Row(children: [
+                      Icon(Icons.work),
+                      SizedBox(width: 6,),
+                      Text(complexityText)
+                    ],),
+                  Row(children: [
+                      Icon(Icons.attach_money),
+                      SizedBox(width: 6,),
+                      Text(affordabilityText)
+                    ],)
+                ],
+              ),
             )
           ],
         ),
