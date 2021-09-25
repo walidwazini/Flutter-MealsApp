01c7c923af2153b4fb0f32354506b91a9cdd6426
Walid Wazini <wazini25@gmail.com>
2021-09-25 23:23:49 +0800

    Convert ingredients item widgets into buldContainer

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -6,50 +6,71 @@ class MealDetailScreen extends StatelessWidget {
   //const MealDetailScreen({Key? key}) : super(key: key);
   static const routeName = '/meal-detail';
 
-  Widget buildSectionTitle(BuildContext ctx, String text){
+  Widget buildSectionTitle(BuildContext ctx, String text) {
     return Container(
       margin: EdgeInsets.symmetric(vertical: 10),
-      child:Text(text, style: Theme.of(ctx).textTheme.headline6,),
+      child: Text(
+        text,
+        style: Theme.of(ctx).textTheme.headline6,
+      ),
+    );
+  }
+
+  //
+  Widget buildContainer(Widget anak) {
+    return Container(
+      height: 150,
+      width: 500,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: anak,
     );
   }
 
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
-    final selectedMeal = DUMMY_MEALS.firstWhere((theMeal) => theMeal.id == mealId);
+    final selectedMeal =
+        DUMMY_MEALS.firstWhere((theMeal) => theMeal.id == mealId);
     return Scaffold(
         appBar: AppBar(
-          title: Text('$mealId ${selectedMeal.title}',),
+          title: Text(
+            '$mealId ${selectedMeal.title}',
+          ),
         ),
         body: Column(
           children: [
             // 1) For image, need to find which id the user selected,
             // --- so need to import the data. and make a condition to compare the id
             Container(
-              height: 300,width:double.infinity,
-              child: Image.network(selectedMeal.imageUrl, fit: BoxFit.cover,),
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                selectedMeal.imageUrl,
+                fit: BoxFit.cover,
+              ),
             ),
             Text('THe meal id - $mealId --'),
             buildSectionTitle(context, 'Ingredients'),
-            Container( height: 150, width: 500,
-              margin: EdgeInsets.all(10),  padding: EdgeInsets.all(5),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(10)
-              ),
-              child: ListView.builder(
+            buildContainer(
+              ListView.builder(
                 itemBuilder: (ctx, index) {
                   return Card(
                     color: Theme.of(context).accentColor,
                     child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 5,horizontal: 10),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 5, horizontal: 10),
                       child: Text(selectedMeal.ingredients[index]),
                     ),
                   );
                 },
                 itemCount: selectedMeal.ingredients.length,
-              ) ,
+              ),
             ),
             buildSectionTitle(context, 'Steps'),
           ],
