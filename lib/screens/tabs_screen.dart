cbdad479f0a82a0c4e96a071a38bdc78499d25df
Walid Wazini <wazini25@gmail.com>
2021-09-26 09:55:45 +0800

    Crerate Tab Screen

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../screens/categories_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  //const TabsScreen({Key? key}) : super(key: key);
+
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(length: 2, child: Scaffold(
+      appBar: AppBar(
+        title: Text('Meals Tab'),
+        bottom: TabBar(tabs: [
+          Tab(icon: Icon(Icons.category),text: 'Categories',),
+          Tab(icon: Icon(Icons.favorite),text: 'Favourites',),
+        ],),
+      ),
+      body: TabBarView(children: [
+        CategoriesScreen(),
+      ],),
+    ));
+  }
+}
