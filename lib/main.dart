bf7d4affc85e2f7264b86b3ba77b914cfa6585fa
Walid Wazini <wazini25@gmail.com>
2021-09-26 10:01:03 +0800

    change default main screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import './screens/category_meals_screen.dart';
 import './screens/categories_screen.dart';
 import './screens/meal_detail_screen.dart';
+import './screens/tabs_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
           headline6: TextStyle(fontSize: 20,fontFamily: 'RobotoCondensed', fontWeight: FontWeight.bold ),
         ),
       ),
-      home: CategoriesScreen(),
+      home: TabsScreen(),
       //initialRoute: '/',
       routes: {
         //'/' : (konteks) => CategoriesScreen(),
