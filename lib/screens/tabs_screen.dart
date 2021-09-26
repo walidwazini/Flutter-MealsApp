42ff558fd86561f5784400c3ca2e6758c873f73f
Walid Wazini <wazini25@gmail.com>
2021-09-26 15:24:00 +0800

    fixed selection for pages

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -11,15 +11,27 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Widget> _pages = [ CategoriesScreen(), FavouritesScreen() ];
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index){
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Meals Tab'),
       ),
-      body: null,
+      body: _pages[_selectedPageIndex],
       bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
         backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor:Theme.of(context).accentColor ,
+        currentIndex: _selectedPageIndex,
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.category_rounded), label: 'Categories', ),
           BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Favourites', ),
